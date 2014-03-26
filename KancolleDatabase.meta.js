f9203ec92ab9d8433cf3acd4a9d19f781747cb6a
Zharay <danvargas404@gmail.com>
2014-03-26 02:57:07 -0400

    wikiwiki seems to semi work... adding it.

--- KancolleDatabase.meta.js
+++ KancolleDatabase.meta.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name		Kancolle Database! Translation
 // @namespace	http://zharay.github.io/KancolleDatabaseEN/
-// @version		1.04
+// @version		1.05
 // @description Translate most of Kancolle Database! Warning: Don't waste all your resources on your daughters.... Okay keep it reasonable.
 // @match		http://*.kancolle-db.tk/*
 // @match		https://*.kancolle-db.tk/*
+// @match		http://www51.atpages.jp/kancollev/*
+// @match		http://*wikiwiki.jp/kancolle/*
 // @require		http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js
 // @grant		none
 // @updateURL	https://github.com/Zharay/KancolleDatabaseEN/raw/master/KancolleDatabase.meta.js
