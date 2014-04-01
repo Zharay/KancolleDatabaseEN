2ab7aabbb77be9897b050a762e83112cba7601d2
Zharay <danvargas404@gmail.com>
2014-04-01 06:06:51 -0400

    atwiki added

--- KancolleDatabase.user.js
+++ KancolleDatabase.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name		Kancolle Database! Translation
 // @namespace	http://zharay.github.io/KancolleDatabaseEN/
-// @version		1.06
+// @version		1.07
 // @description Translate most of Kancolle Database! Warning: Don't waste all your resources on your daughters.... Okay keep it reasonable.
 // @match		http://*.kancolle-db.tk/*
 // @match		https://*.kancolle-db.tk/*
-// @match		http://www51.atpages.jp/kancollev/*
+// @match		http://*.atpages.jp/kancollev/*
 // @match		http://*wikiwiki.jp/kancolle/*
+// @match		http://*.atwiki.jp/kancolle/*
 // @require		http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js
 // @grant		none
 // @updateURL	https://github.com/Zharay/KancolleDatabaseEN/raw/master/KancolleDatabase.meta.js
